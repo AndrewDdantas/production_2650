f494dd20fee1d8a81cc5f197105eb05c19d73821
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-07-03 00:22:28 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from time import sleep
+import services.connect as C
+
+st.title("Bem vindo ao app de recolhimentos do Magazine Luiza CD 2650.")
+
+with st.form('LoginForm'):
+    User = st.number_input('CD',step=1)
+
+    Pass = st.text_input("Senha")
+
+    login_button = st.form_submit_button("Login")
+
+    if login_button:
+        if Pass == st.secrets['senha']:
+            st.session_state['Usuário'] = User
+            st.session_state['Login'] = 'Logado'
+            st.warning('Login realizado com sucesso!')
+            sleep(2)
+            st.switch_page('./pages/connect.py')
+            
+        else:
+            st.error("Favor verificar os dados.")
